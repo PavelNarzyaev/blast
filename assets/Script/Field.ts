fa869469c32a9e07064c6f0d21796318dfbd1753
Pavel Narzyaev <pavel@narzyaev.ru>
2019-11-21 16:10:57 +0300

    added prefabs in Field.ts component

--- assets/Script/Field.ts
+++ assets/Script/Field.ts
@@ -9,6 +9,9 @@ export default class Field extends cc.Component {
 	@property
 	columnsNum: number = 0;
 
+	@property([cc.Prefab])
+	blocksPrefabs: cc.Prefab[] = [];
+
 	start() {
 		this.node.height = this.columnsNum / this.rowsNum * this.node.width;
 	}
